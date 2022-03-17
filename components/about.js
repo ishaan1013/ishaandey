6a9d2c7f204e48b8af36405604573f55e5c064c8
ishaan1013 <ishaan1013@gmail.com>
2022-03-16 22:15:44 -0400

    about scrolling text + placeholder text

--- components/about.js
+++ components/about.js
@@ -3,7 +3,22 @@ import styles from '../styles/Home.module.scss'
 export default function About() {
     return (
         <section className={styles.about}>
-
+            <div className={styles.abtFlex}>
+                <div className={styles.left}>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>
+                <div className={styles.right}>
+                    <div className={styles.trackRotate}>
+                        <div className={styles.track}>
+                            <div>
+                                &nbsp;About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me  -  About Me
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
