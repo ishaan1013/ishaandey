502fa7683bd142a1fa9b7804ac91f5542d69c25a
ishaan1013 <ishaan1013@gmail.com>
2022-03-22 21:52:09 -0400

    card content (tools, link)
    also reduce image sizes

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -9,6 +9,10 @@ import taDisplay3 from '../assets/ta5.png'
 import ohseaDisplay1 from '../assets/ohsea1.png'
 import ohseaDisplay2 from '../assets/ohsea3.png'
 
+import { IoLogoAppleAppstore } from 'react-icons/io5'
+import { IoLogoGithub } from 'react-icons/io5'
+import { HiOutlineExternalLink } from 'react-icons/hi'
+
 export default function Portfolio() {
     return (
         <>
@@ -35,7 +39,7 @@ export default function Portfolio() {
                                             width={107}
                                             height={225}
                                             alt="teachassist app 1"
-                                            placeholder="blur"
+                                            priority
                                         />
                                     </div>
                                     <div className={`${styles.taDisplay3} ${styles.taDisplayItem}`}>
@@ -44,7 +48,7 @@ export default function Portfolio() {
                                             width={107}
                                             height={225}
                                             alt="teachassist app 1"
-                                            placeholder="blur"
+                                            priority
                                         />
                                     </div>
                                 </div>
@@ -52,6 +56,14 @@ export default function Portfolio() {
                             <div className={styles.lower}>
                                 <h3>TeachAssist App</h3>
                                 <p>Advanced tools for high school students in Ontario to promote academic success.</p>
+                                <div className={styles.tools}>
+                                    <div className={styles.tool}>React Native</div>
+                                    <div className={styles.tool}>Expo</div>
+                                </div>
+                                <div className={styles.links}>
+                                    <a href="https://github.com/Teach-Assist-App/frontend2" target="_blank" className={styles.link}><IoLogoGithub/></a>
+                                    <a target="_blank" className={styles.link}><IoLogoAppleAppstore/></a>
+                                </div>
                             </div>
                         </div>
                         <div className={`${styles.gridItem} ${styles.gI2}`}>
@@ -80,6 +92,15 @@ export default function Portfolio() {
                             <div className={styles.lower}>
                                 <h3>OHSEA</h3>
                                 <p>The Ontario High School Esports Association leads in high school esports opportunities.</p>
+                                <div className={styles.tools}>
+                                    <div className={styles.tool}>React</div>
+                                    <div className={styles.tool}>Gatsby</div>
+                                    <div className={styles.tool}>SCSS</div>
+                                </div>
+                                <div className={styles.links}>
+                                    <a href="https://github.com/ohseagg/ohsea.gg" target="_blank" className={styles.link}><IoLogoGithub/></a>
+                                    <a href="https://ohsea.gg/" target="_blank" className={styles.link}><HiOutlineExternalLink/></a>
+                                </div>
                             </div>
                         </div>
                         <div className={`${styles.gridItem} ${styles.gI3}`}>
