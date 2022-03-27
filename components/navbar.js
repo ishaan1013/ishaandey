e5ac5dd45c30da2db09e88de5c27687fd3a9ae3d
ishaan1013 <ishaan1013@gmail.com>
2022-03-27 16:58:11 -0400

    responsive navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-
+import { useState } from 'react'
+import { IoMenu, IoClose } from "react-icons/io5";
 
 export default function Navbar() {
     const { asPath } = useRouter()
+    const [visible, setVisible] = useState(false);
 
     const scroll = () => {
         window.scroll({
@@ -28,15 +30,51 @@ export default function Navbar() {
         }
     }
 
+    function Toggle() {
+        if (visible) {
+            return <IoClose onClick={() => setVisible(false)}/>
+        }
+        else {
+            return <IoMenu onClick={() => setVisible(true)}/>
+        }
+    }
+
+    function NavMobile() {
+        if (visible) {
+            return (
+                <nav className="nav2">
+                    <div className="container">
+                        <ul>
+                        <li>
+                            <AboutNav/>
+                        </li>
+                        <li><Link href="/portfolio">Portfolio</Link></li>
+                        <li><Link href="/contact">Contact</Link></li>
+                        </ul>
+                    </div>
+                </nav>
+            )
+        }
+        else {
+            return null
+        }
+    }
+
     return (
-        <nav>
-            <ul>
-            <li>
-                <AboutNav/>
-            </li>
-            <li><Link href="/portfolio">Portfolio</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
-            </ul>
-        </nav>
+        <>
+            <nav className="nav1">
+                <ul>
+                <li>
+                    <AboutNav/>
+                </li>
+                <li><Link href="/portfolio">Portfolio</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+            <div className="toggle">
+                <Toggle/>
+            </div>
+            <NavMobile/>
+        </>
     )
 }
